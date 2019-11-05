d385f11ca6b8a268a645f04d0a65e308ef32ed6d
Zachary Cutlip <uid000@gmail.com>
2019-11-05 10:45:01 -0800

    whitespace fixes

--- make_c/command_line.py
+++ make_c/command_line.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-
 from make_c import make_c
 
 
-
-
-
 def main():
-    argv=sys.argv[1:]
+    argv = sys.argv[1:]
     make_c.main(argv)
-    
-
--- make_c/make_c.py
+++ make_c/make_c.py
@@ -5,66 +5,71 @@ import os
 import subprocess
 import sys
 
-CProgramClasses={}
+CProgramClasses = {}
+
 
 def register(cls):
-    newclass=cls
-    CProgramClasses[newclass.EDITOR]=newclass
+    newclass = cls
+    CProgramClasses[newclass.EDITOR] = newclass
     return newclass
 
+
 class CProgramMetaClass(type):
-    def __new__(cls,clsname,bases,attrs):
-        newclass=super(CProgramMetaClass,cls).__new__(cls,clsname,bases,attrs)
+    def __new__(cls, clsname, bases, attrs):
+        newclass = super(CProgramMetaClass, cls).__new__(
+            cls, clsname, bases, attrs)
         register(newclass)
         return newclass
 
+
 class CProgram(metaclass=CProgramMetaClass):
-    __metaclass__=CProgramMetaClass
-    FOURSPACES="    "
-    TAB="\t"
-    INSERT_CODE_HERE="//insert code here"
-    EDITOR="vim"
-    def __init__(self,filename,spaces=True,generate_makefile=False,run_editor=True):
-        self.filename=filename
+    __metaclass__ = CProgramMetaClass
+    FOURSPACES = "    "
+    TAB = "\t"
+    INSERT_CODE_HERE = "//insert code here"
+    EDITOR = "vim"
+
+    def __init__(self, filename, spaces=True, generate_makefile=False, run_editor=True):
+        self.filename = filename
         if spaces:
-            tab=self.FOURSPACES
+            tab = self.FOURSPACES
         else:
-            tab=self.TAB
+            tab = self.TAB
 
-        self.insert_code_comment=tab+self.INSERT_CODE_HERE
-        lines= ["#include <stdlib.h>",
-                "#include <stdio.h>",
-                "#include <unistd.h>",
-                "#include <string.h>",
-                "",
-                "",
-                "int main(int argc, char **argv)",
-                "{",
-                tab+"int ret=0;",
-                "",
-                tab+"printf(\"Hello world\\n\");",
-                self.insert_code_comment, #so we can dyanmically locate its index later
-                "",
-                tab+"return ret;",
-                "}",
-                ""]
-        self.lines=lines
-        self.edit_line=self.lines.index(self.insert_code_comment)+1
-        self.edit_column=len(tab)+1
+        self.insert_code_comment = tab + self.INSERT_CODE_HERE
+        lines = ["#include <stdlib.h>",
+                 "#include <stdio.h>",
+                 "#include <unistd.h>",
+                 "#include <string.h>",
+                 "",
+                 "",
+                 "int main(int argc, char **argv)",
+                 "{",
+                 tab + "int ret=0;",
+                 "",
+                 tab + "printf(\"Hello world\\n\");",
+                 self.insert_code_comment,  # so we can dyanmically locate its index later
+                 "",
+                 tab + "return ret;",
+                 "}",
+                 ""]
+        self.lines = lines
+        self.edit_line = self.lines.index(self.insert_code_comment) + 1
+        self.edit_column = len(tab) + 1
 
-        self._write_to_file(self.filename,self.lines)
+        self._write_to_file(self.filename, self.lines)
         if generate_makefile:
-            self.makefile_lines=self._generate_makefile()
-            self._write_to_file("Makefile",self.makefile_lines)
+            self.makefile_lines = self._generate_makefile()
+            self._write_to_file("Makefile", self.makefile_lines)
 
         if run_editor:
             self.open_in_editor()
 
-    def _write_to_file(self,filename,lines):
+    def _write_to_file(self, filename, lines):
         try:
-            with open(filename,"w") as outfile:
+            with open(filename, "w") as outfile:
                 for line in lines:
-                    outfile.write(line+"\n")
+                    outfile.write(line + "\n")
 
         except Exception as e:
             print("Error writing %s" % self.filename)
@@ -72,95 +77,105 @@ class CProgram(metaclass=CProgramMetaClass):
             raise
 
     def _generate_makefile(self):
-        basename=os.path.splitext(self.filename)[0]
+        basename = os.path.splitext(self.filename)[0]
 
-        lines=[ "%s:%s" %(basename,self.filename),
-                "\tcc $^ -o $@",
-                "",
-                "clean:",
-                "\t-rm -f %s" % basename,
-                ""]
+        lines = ["%s:%s" % (basename, self.filename),
+                 "\tcc $^ -o $@",
+                 "",
+                 "clean:",
+                 "\t-rm -f %s" % basename,
+                 ""]
 
         return lines
 
     def generate_editor_command(self):
-        line_column_arg="+call cursor(%d,%d)" % (self.edit_line,self.edit_column)
+        line_column_arg = "+call cursor(%d,%d)" % (self.edit_line,
+                                                   self.edit_column)
 
-        #vim foo.c "+call cursor(4,5)"
-        return[self.EDITOR,line_column_arg,self.filename]
-    
+        # vim foo.c "+call cursor(4,5)"
+        return[self.EDITOR, line_column_arg, self.filename]
+
     def open_in_editor(self):
-        p=subprocess.Popen(self.generate_editor_command())
+        p = subprocess.Popen(self.generate_editor_command())
         p.wait()
 
-class CProgramWithSublime(CProgram):
-    EDITOR="subl"
-    def generate_editor_command(self):
-        line_column_arg=":%d:%d" % (self.edit_line,self.edit_column)
 
-        #subl foo.c:4:5
-        editor_cmd=[self.EDITOR,self.filename+line_column_arg]
-        
+class CProgramWithSublime(CProgram):
+    EDITOR = "subl"
+
+    def generate_editor_command(self):
+        line_column_arg = ":%d:%d" % (self.edit_line, self.edit_column)
+
+        # subl foo.c:4:5
+        editor_cmd = [self.EDITOR, self.filename + line_column_arg]
+
         return editor_cmd
+
 
 class CProgramWithTextMate(CProgram):
-    EDITOR="mate"
+    EDITOR = "mate"
 
     def generate_editor_command(self):
-        line_column_arg="%d:%d" % (self.edit_line,self.edit_column)
+        line_column_arg = "%d:%d" % (self.edit_line, self.edit_column)
 
-        #mate -l 4:5 foo.c
-        editor_cmd=[self.EDITOR,"-l",line_column_arg,self.filename]
+        # mate -l 4:5 foo.c
+        editor_cmd = [self.EDITOR, "-l", line_column_arg, self.filename]
         return editor_cmd
+
 
 def list_editors():
     print("Known editors:")
     for editor in CProgramClasses.keys():
         print("%s" % editor)
 
-def parse_args(argv):
-    parser=argparse.ArgumentParser()
-    grp=parser.add_mutually_exclusive_group(required=True)
-    grp.add_argument("filename",nargs='?',help="Name of the source file to create.",default=None)
-    grp.add_argument("--list-editors",help="List known editors.",action="store_true",default=False)
-    parser.add_argument("--editor",help="Editor to use to open the resulting source file.",default="vim")
-    parser.add_argument("--skip-editor",help="Create the source file but don't open it in an editor.",action="store_true")
-    parser.add_argument("--tabs",help="Use tabs instead of spaces.",action="store_true")
-    parser.add_argument("--generate-makefile",help="Create a makefile to build the program.",action="store_true")
 
-    args=parser.parse_args(argv)
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    grp = parser.add_mutually_exclusive_group(required=True)
+    grp.add_argument("filename", nargs='?',
+                     help="Name of the source file to create.", default=None)
+    grp.add_argument("--list-editors", help="List known editors.",
+                     action="store_true", default=False)
+    parser.add_argument(
+        "--editor", help="Editor to use to open the resulting source file.", default="vim")
+    parser.add_argument(
+        "--skip-editor", help="Create the source file but don't open it in an editor.", action="store_true")
+    parser.add_argument(
+        "--tabs", help="Use tabs instead of spaces.", action="store_true")
+    parser.add_argument("--generate-makefile",
+                        help="Create a makefile to build the program.", action="store_true")
+
+    args = parser.parse_args(argv)
     return args
 
+
 def main(argv):
-    args=parse_args(argv)
-    run_editor=True
+    args = parse_args(argv)
+    run_editor = True
 
     if args.list_editors:
         list_editors()
         exit(0)
 
+    run_editor = not args.skip_editor
 
-    run_editor=not args.skip_editor
+    filename = args.filename
 
-    filename=args.filename
-    
-    
-    
-    editor=args.editor
+    editor = args.editor
 
-    spaces=not args.tabs
-    generate_makefile=args.generate_makefile
+    spaces = not args.tabs
+    generate_makefile = args.generate_makefile
     try:
-        c_program_class=CProgramClasses[editor]
-    except:
+        c_program_class = CProgramClasses[editor]
+    except Exception:
         print("Unknown editor: %s" % editor)
         print("Consider using --skip-editor to create source file without editing.")
+        raise
         exit(1)
-    
-    c_program_class(filename,spaces=spaces,generate_makefile=generate_makefile,run_editor=run_editor)
 
-if __name__=="__main__":
+    c_program_class(filename, spaces=spaces,
+                    generate_makefile=generate_makefile, run_editor=run_editor)
+
+
+if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-
