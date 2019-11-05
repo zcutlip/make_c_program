0fa3fe56531bf1354ae1088841eb70d5b3cc3383
Zachary Cutlip <uid000@gmail.com>
2019-11-05 11:12:39 -0800

    enhanced setup.py for pypi happiness

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 
-setup(name='make_c',
-      version='0.1',
-      description='A utility to create a simple ' +
-      'C source file with a main and open it in an editor.',
-      url="TBD",
+about = {}
+
+with open("make_c/__about__.py") as fp:
+    exec(fp.read(), about)
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Zachary Cutlip",
+      author_email="uid000@gmail.com",
+      license="MIT",
+      url="https://github.com/zcutlip/make_c_program",
       packages=['make_c'],
       entry_points={
-        'console_scripts': ['make_c=make_c.command_line:main'],
-        },
-      python_requires='>=3.3'
-      )
+          'console_scripts': ['make_c=make_c.command_line:main'],
+},
+    python_requires='>=3.3'
+)
