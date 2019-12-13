14f1037e8c50ae3361b1bc829bc8384f42cd479c
Zachary Cutlip <uid000@gmail.com>
2019-12-13 13:04:48 +1100

    export UnknownEditorException class

--- make_c/__init__.py
+++ make_c/__init__.py
@@ -4,10 +4,12 @@ from .__about__ import (
     __summary__
 )
 from .editors import CProgramClasses
+from .meta import UnknownEditorException
 
 __all__ = [
     "__version__",
     "__title__",
     "__summary__",
-    CProgramClasses
+    CProgramClasses,
+    UnknownEditorException
 ]
