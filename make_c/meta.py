877420aa89db37bfdb010e13c7cdf8c860c44bfa
Zachary Cutlip <uid000@gmail.com>
2019-12-13 13:02:51 +1100

    refactor CProgramClasses to raise custom exception for unknown editor

--- make_c/meta.py
+++ make_c/meta.py
@@ -1,15 +1,24 @@
-CProgramClasses = {}
+class UnknownEditorException(Exception):
+    pass
 
 
-def register(cls):
-    newclass = cls
-    CProgramClasses[newclass.EDITOR] = newclass
-    return newclass
+class CProgramClasses:
+    _classes = {}
+
+    @classmethod
+    def register_new(cls, editor_name, editor_class):
+        cls._classes[editor_name] = editor_class
+
+    @classmethod
+    def lookup_editor(cls, editor_name):
+        if editor_name not in cls._classes:
+            raise UnknownEditorException()
+        return cls._classes[editor_name]
 
 
 class CProgramMetaClass(type):
     def __new__(cls, clsname, bases, attrs):
         newclass = super(CProgramMetaClass, cls).__new__(
             cls, clsname, bases, attrs)
-        register(newclass)
+        CProgramClasses.register_new(newclass.EDITOR, newclass)
         return newclass
