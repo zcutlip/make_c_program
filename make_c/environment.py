e42a157a52f9afd1b8b011b41e3da9fe2431257f
Zachary Cutlip <uid000@gmail.com>
2020-01-14 15:23:07 -0800

    support more environment variables
    1. MAKE_C_EDITOR
    2. EDITOR
    3. VISUAL

--- make_c/environment.py
+++ make_c/environment.py
@@ -1,11 +1,23 @@
 import os
 
+MAKE_C_EDITOR_ENV_VAR = "MAKE_C_EDITOR"
+EDITOR_ENV_VAR = "EDITOR"
+VISUAL_ENV_VAR = "VISUAL"
+
 
 class EditorFromEnv:
+    ENV_VAR_NAMES = [
+        MAKE_C_EDITOR_ENV_VAR,
+        EDITOR_ENV_VAR,
+        VISUAL_ENV_VAR
+    ]
+
     def __init__(self):
         self.editor = None
         self.path = None
-        env_editor = os.environ.get("EDITOR")
-        if env_editor:
-            self.path = os.path.dirname(env_editor)
-            self.editor = os.path.basename(env_editor)
+        for env_var_name in self.ENV_VAR_NAMES:
+            env_editor = os.environ.get(env_var_name)
+            if env_editor:
+                self.path = os.path.dirname(env_editor)
+                self.editor = os.path.basename(env_editor)
+                break
